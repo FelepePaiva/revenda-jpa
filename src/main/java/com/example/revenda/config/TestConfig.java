c61ba3cb7b73a232cd16871cd19956a2c18dc99e
Felepe Paiva <felipe.geo.uece@gmail.com>
2023-05-27 16:38:37 -0300

    JPA repository, dependecy injection and database seeding

---
+++ src/main/java/com/example/revenda/config/TestConfig.java
@@ -0,0 +1,28 @@
+package com.example.revenda.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.example.revenda.entities.Vehicle;
+import com.example.revenda.repositories.VehicleRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+	@Autowired
+	private VehicleRepository vehicleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Vehicle v1 = new Vehicle(null, "Ônix", "Chevrolet", "Vermelho", 2020, 68000.0);
+		Vehicle v2 = new Vehicle(null, "HB20", "Hyundai", "Branco", 2019, 62000.0);
+		
+		vehicleRepository.saveAll(Arrays.asList(v1, v2));
+		
+	}
+
+}
---
+++ src/main/java/com/example/revenda/repositories/VehicleRepository.java
@@ -0,0 +1,9 @@
+package com.example.revenda.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.revenda.entities.Vehicle;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long>{
+
+}
